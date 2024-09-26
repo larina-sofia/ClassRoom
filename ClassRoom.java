add8140a5f31921acd295666725f7c546970351d
Sofia Larina <lar240747@spengergasse.at>
2024-09-26 11:55:01 +0200

    Add: beamer and windows

--- ClassRoom.java
+++ ClassRoom.java
@@ -3,7 +3,9 @@ public class ClassRoom
     private String  roomNr;
     private boolean computer;
     private int     size;
-         
+    private boolean beamer;
+    private boolean windows;
+    
     public void setRoomNr(String newRoomNr)
     {
         roomNr = newRoomNr;
@@ -19,6 +21,16 @@ public class ClassRoom
         size = newSize;
     }
     
+    public void setBeamer(boolean newBeamer)
+    {
+        beamer = newBeamer;
+    }
+    
+    public void setWindows(boolean newWindows)
+    {
+        windows = newWindows;
+    }
+    
     public String getRoomNr()
     {
         return roomNr;
@@ -32,5 +44,15 @@ public class ClassRoom
     public int getSize()
     {
         return size;
+    }
+    
+    public boolean getBeamer()
+    {
+        return beamer;
+    }
+    
+    public boolean getWindows()
+    {
+        return windows;
     }
 }
